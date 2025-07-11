ca7b903e9dc92bbbce6095b75a3803cee0eb0dc4
Shivu384 <shivamarora382004@gmail.com>
2025-07-12 00:10:52 +0530

    adding chatbot faq component with removing existing errors

--- App.jsx
+++ App.jsx
@@ -8,6 +8,7 @@ import AdminDashboard from './components/AdminDashboard';
 import Error404 from './components/Error404';
 import Footer from "./components/Footer";
 import ScrollToTop from './components/ScrollToTop';
+import ChatBot from './components/ChatBot'; // Import the ChatBot component
 
 // Newly added pages
 import About from "./Pages/About";
@@ -24,45 +25,50 @@ const App = () => {
   return (
     <BrowserRouter>
       <ScrollToTop />
-      <Routes>
-        {/* Public routes */}
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/privacy" element={<Privacy />} />
-        <Route path="/terms" element={<Terms />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/report-issue" element={<ReportIssue />} />        
-        <Route path="*" element={<Error404 />} />
-        <Route path='/download-android' element={<DownloadAndroid/>}/>
-        <Route path='/download-ios' element={<DownloadIOS/>}/>
-        <Route path="/community-voting" element={<CommunityVotingPage />} />
+      {/* Main app content with routes */}
+      <div className="relative min-h-screen">
+        <Routes>
+          {/* Public routes */}
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/privacy" element={<Privacy />} />
+          <Route path="/terms" element={<Terms />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/report-issue" element={<ReportIssue />} />        
+          <Route path="*" element={<Error404 />} />
+          <Route path='/download-android' element={<DownloadAndroid/>}/>
+          <Route path='/download-ios' element={<DownloadIOS/>}/>
+          <Route path="/community-voting" element={<CommunityVotingPage />} />
 
-        {/* Protected routes */}
-        <Route 
-          path="/admin/dashboard" 
-          element={
-            <PrivateRoute allowedRoles={['admin']}>
-              <AdminDashboard />
-            </PrivateRoute>
-          } 
-        />
-        <Route 
-          path="/home" 
-          element={
-            <PrivateRoute allowedRoles={['user', 'admin']}>
-              <Home />
-            </PrivateRoute>
-          } 
-        />
+          {/* Protected routes */}
+          <Route 
+            path="/admin/dashboard" 
+            element={
+              <PrivateRoute allowedRoles={['admin']}>
+                <AdminDashboard />
+              </PrivateRoute>
+            } 
+          />
+          <Route 
+            path="/home" 
+            element={
+              <PrivateRoute allowedRoles={['user', 'admin']}>
+                <Home />
+              </PrivateRoute>
+            } 
+          />
 
-      <Route path="/500" element={<ServerError />} />
+          <Route path="/500" element={<ServerError />} />
+        </Routes>
 
-      </Routes>
-
-      {/* Footer */}
-     
+        {/* Footer */}
+        <Footer />
+        
+        {/* ChatBot Component - will appear on all pages */}
+        <ChatBot />
+      </div>
     </BrowserRouter>
   );
 };
